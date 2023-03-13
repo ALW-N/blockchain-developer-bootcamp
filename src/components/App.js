01be1fa5e2996cd0f1e4d967187c4ffaa409433a
ALW-N <alwintomy11@gmail.com>
2023-03-13 09:32:02 +0530

    Trades

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import Balance from "./Balance.js";
 import Order from "./Order.js";
 import OrderBook from "./OrderBook.js";
 import PriceChart from "./PriceChart.js";
+import Trades from "./Trades.js";
 
 function App() {
   const dispatch = useDispatch();
@@ -69,7 +70,7 @@ function App() {
 
           {/* Transactions */}
 
-          {/* Trades */}
+          <Trades />
 
           <OrderBook/>
 
