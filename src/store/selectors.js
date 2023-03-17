e6c05df2ca3f054a1f57c0c240e9e4ddf8630760
ALW-N <alwintomy11@gmail.com>
2023-03-18 00:03:09 +0530

    Transaction Status

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -7,6 +7,7 @@ const GREEN = '#25CE8F'
 const RED = '#F45353'
 
 const tokens = state => get(state, 'tokens.contracts')
+const events = state => get(state, 'exchange.events')
 const account = state => get(state, 'provider.account')
 const allOrders = state => get(state, 'exchange.allOrders.data', [])
 const cancelledOrders = state => get(state, 'exchange.cancelledOrders.data', [])
@@ -25,6 +26,12 @@ const openOrders = state => {
     })
     return openOrders
 }
+
+export const myEventsSelector = createSelector( account, events, (account, events) => {
+    events = events.filter((e)=> e.args.user === account)
+
+    return events
+})
 
 export const myOpenOrdersSelector = createSelector(
     account,
@@ -95,16 +102,19 @@ const decorateOrder = (order, tokens) => {
     })
 }
 
-
 export const filledOrdersSelector = createSelector(
-    filledOrders, tokens, (orders, tokens) => {
+    filledOrders,
+    tokens,
+    (orders, tokens) => {
         if (!tokens[0] || !tokens[1]) { return }
 
-        // Filter orders by selected tokens
         orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
         orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
         orders = orders.sort((a, b) => a.timestamp - b.timestamp)
+
         orders = decorateFilledOrders(orders, tokens)
+
         orders = orders.sort((a, b) => b.timestamp - a.timestamp)
 
         return orders
@@ -127,7 +137,7 @@ const decorateFilledOrders = (orders, tokens) => {
 const decorateFilledOrder = (order, previousOrder) => {
     return ({
         ...order,
-        tokenPriceClass: tokenPriceClass(order.tokePrice, order.id, previousOrder),
+        tokenPriceClass: tokenPriceClass(order.tokenPrice, order.id, previousOrder)
     })
 }
 
@@ -135,13 +145,14 @@ const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
     if (previousOrder.id === orderId) {
         return GREEN
     }
+
     if (previousOrder.tokenPrice <= tokenPrice) {
         return GREEN
     } else {
         return RED
     }
 }
-///
+
 export const myFilledOrdersSelector = createSelector(
     account,
     tokens,
@@ -190,7 +201,6 @@ const decorateMyFilledOrder = (order, account, tokens) => {
     })
 }
 
-
 export const orderBookSelector = createSelector(
     openOrders,
     tokens,
@@ -201,7 +211,6 @@ export const orderBookSelector = createSelector(
         orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
         orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
 
-        // Decorate orders
         orders = decorateOrderBookOrders(orders, tokens)
 
         orders = groupBy(orders, 'orderType')
@@ -221,7 +230,6 @@ export const orderBookSelector = createSelector(
         }
         return orders
     }
-
 )
 
 const decorateOrderBookOrders = (orders, tokens) => {
